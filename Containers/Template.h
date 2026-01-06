832de8d2c6afa13b777d20d0a9beac5d1bf41253
miroslavmadzharov02 <miroslavmadzharov02@gmail.com>
2026-01-06 12:45:26 +0200

    teamplate for other containers

---
+++ Containers/Template.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Template
+{
+private:
+	int* arr;
+public:
+	void initializing() {}
+
+	void traversal() {}
+
+	void addElement(int x) {}
+
+	void insertElements() {}
+
+	void erase() {}
+
+	void getElement() {}
+
+	void querySize() {}
+};
+
